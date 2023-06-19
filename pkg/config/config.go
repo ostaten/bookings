1ba0eb6ed2c849e9a2ee5ff1f6f0184b81e03e28
Oliver Staten <statenoz98@gmail.com>
2023-06-19 10:30:44 -0700

    initial commit

---
+++ pkg/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"html/template"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+//config might be accessed from any part of application
+//should not import anything; only should be imported
+
+// AppConfig hold the application config
+type AppConfig struct {
+	UseCache      bool
+	TemplateCache map[string]*template.Template
+	InProduction bool
+	Session *scs.SessionManager
+}
