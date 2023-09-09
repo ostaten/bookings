0234d9aee471de9c6ba2e78342913bb5119a108d
Oliver Staten <statenoz98@gmail.com>
2023-09-09 02:52:25 -0700

    user table migration

---
+++ migrations/20230909093631_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES('Oliver', 'Staten', 'admin@admin.com', '$2a$12$VRe5OFKAkvqgAoX/Y9zmguVnXo/qNn/2BLl/yDd4Faq0RMLdxLmK2', 3, '2023-09-02 00:00:00.000', '2023-09-02 00:00:00.000');
